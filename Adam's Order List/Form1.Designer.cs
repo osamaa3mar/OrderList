c860e9c5259d04ee53b07c4b6576259069c1b234
osamaa3mar <53447224+osamaa3mar@users.noreply.github.com>
2023-04-23 13:50:36 +0300

    Add files via upload

---
+++ Adam's Order List/Form1.Designer.cs
@@ -0,0 +1,309 @@
+﻿namespace Adam_s_Order_List
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.ListViewGroup listViewGroup7 = new System.Windows.Forms.ListViewGroup("قرطاسية", System.Windows.Forms.HorizontalAlignment.Left);
+            System.Windows.Forms.ListViewGroup listViewGroup8 = new System.Windows.Forms.ListViewGroup("العاب", System.Windows.Forms.HorizontalAlignment.Left);
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.rbGames = new System.Windows.Forms.RadioButton();
+            this.rbSchool = new System.Windows.Forms.RadioButton();
+            this.button4 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.printPreviewDialog1 = new System.Windows.Forms.PrintPreviewDialog();
+            this.printDialog1 = new System.Windows.Forms.PrintDialog();
+            this.niCopied = new System.Windows.Forms.NotifyIcon(this.components);
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.panel1.Controls.Add(this.rbGames);
+            this.panel1.Controls.Add(this.rbSchool);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.numericUpDown1);
+            this.panel1.Controls.Add(this.btnAdd);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Location = new System.Drawing.Point(592, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(207, 509);
+            this.panel1.TabIndex = 0;
+            // 
+            // rbGames
+            // 
+            this.rbGames.AutoSize = true;
+            this.rbGames.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbGames.Location = new System.Drawing.Point(121, 202);
+            this.rbGames.Name = "rbGames";
+            this.rbGames.Size = new System.Drawing.Size(56, 20);
+            this.rbGames.TabIndex = 7;
+            this.rbGames.TabStop = true;
+            this.rbGames.Text = "العاب";
+            this.rbGames.UseVisualStyleBackColor = true;
+            // 
+            // rbSchool
+            // 
+            this.rbSchool.AutoSize = true;
+            this.rbSchool.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbSchool.Location = new System.Drawing.Point(41, 202);
+            this.rbSchool.Name = "rbSchool";
+            this.rbSchool.Size = new System.Drawing.Size(74, 20);
+            this.rbSchool.TabIndex = 6;
+            this.rbSchool.TabStop = true;
+            this.rbSchool.Text = "قرطاسية";
+            this.rbSchool.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(52, 424);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(109, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "فتح الملف";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Adam_s_Order_List.Properties.Resources.adam_logo;
+            this.pictureBox1.Location = new System.Drawing.Point(41, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(129, 125);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkRed;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Arabic Typesetting", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(26, 324);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(153, 48);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "ازالة";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown1.Location = new System.Drawing.Point(41, 471);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown1.TabIndex = 1;
+            this.numericUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Visible = false;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.DarkGreen;
+            this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnAdd.Font = new System.Drawing.Font("Arabic Typesetting", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.btnAdd.ForeColor = System.Drawing.Color.White;
+            this.btnAdd.Location = new System.Drawing.Point(26, 246);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(153, 48);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "اضافة";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(1, 142);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.textBox1.Size = new System.Drawing.Size(206, 44);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Black;
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.button3);
+            this.panel2.Controls.Add(this.listView1);
+            this.panel2.Location = new System.Drawing.Point(0, 1);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(594, 512);
+            this.panel2.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.DimGray;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(110, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(111, 25);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "نسخ الكل";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.DimGray;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(3, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(101, 25);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Refresh";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.AllowColumnReorder = true;
+            this.listView1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1});
+            this.listView1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listView1.ForeColor = System.Drawing.Color.Black;
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            listViewGroup7.Header = "قرطاسية";
+            listViewGroup7.Name = "قرطاسية";
+            listViewGroup8.Header = "العاب";
+            listViewGroup8.Name = "العاب";
+            this.listView1.Groups.AddRange(new System.Windows.Forms.ListViewGroup[] {
+            listViewGroup7,
+            listViewGroup8});
+            this.listView1.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.listView1.RightToLeftLayout = true;
+            this.listView1.ShowItemToolTips = true;
+            this.listView1.Size = new System.Drawing.Size(594, 509);
+            this.listView1.TabIndex = 4;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "الاسم";
+            this.columnHeader1.Width = 300;
+            // 
+            // printPreviewDialog1
+            // 
+            this.printPreviewDialog1.AutoScrollMargin = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.ClientSize = new System.Drawing.Size(400, 300);
+            this.printPreviewDialog1.Enabled = true;
+            this.printPreviewDialog1.Icon = ((System.Drawing.Icon)(resources.GetObject("printPreviewDialog1.Icon")));
+            this.printPreviewDialog1.Name = "printPreviewDialog1";
+            this.printPreviewDialog1.Visible = false;
+            // 
+            // printDialog1
+            // 
+            this.printDialog1.UseEXDialog = true;
+            // 
+            // niCopied
+            // 
+            this.niCopied.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.niCopied.BalloonTipText = "تم النسخ";
+            this.niCopied.Text = "notifyIcon1";
+            this.niCopied.Visible = true;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnAdd;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 506);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "مكتبة أدم للطلبات";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PrintPreviewDialog printPreviewDialog1;
+        private System.Windows.Forms.PrintDialog printDialog1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.RadioButton rbGames;
+        private System.Windows.Forms.RadioButton rbSchool;
+        private System.Windows.Forms.NotifyIcon niCopied;
+    }
+}
+
---
+++ Adam's Order List/Form1.cs
@@ -0,0 +1,201 @@
+﻿using Adam_s_Order_List.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Adam_s_Order_List
+{
+    public partial class Form1 : Form
+    {
+        //********************Important***************************
+        //change the path of the file from here
+        string path = "C:\\Users\\ayhab\\source\\repos\\Adam's Order List\\Resources\\TextFile.txt";
+        
+        
+        public Form1()
+        {
+            InitializeComponent();
+            LoadAllFileContentToListView();
+        }
+        void LoadAllFileContentToListView()
+        {
+            listView1.Items.Clear();
+            string [] lines = File.ReadAllLines(path);
+            foreach (string line in lines)
+            {
+                string []record = line.Split(',');
+                ListViewItem item = new ListViewItem(record[0]);
+                item.Group = ChosenGroup(record[1]);
+                listView1.Items.Add(item);
+            }
+        }
+        RadioButton CheckedRadio(string record="")
+        {
+            
+            if (rbSchool.Checked || record == "rbSchool")
+            {
+                return rbSchool;
+            }
+            if (rbGames.Checked || record == "rbGames")
+            {
+                return rbGames;
+            }
+            return  rbSchool;
+        }
+        ListViewGroup ChosenGroup(string record = "")
+        {
+            if (rbSchool.Checked || record == "rbSchool")
+            {
+                return listView1.Groups[0];
+            }
+            if (rbGames.Checked || record == "rbGames")
+            {
+                return  listView1.Groups[1];
+            }
+            return listView1.Groups[0];
+        }
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                MessageBox.Show("أضف طلب أولا");
+                textBox1.Text = "";
+                textBox1.Focus();                
+                return;
+            }
+            if( (rbGames.Checked == false && rbSchool.Checked == false))
+            {
+                MessageBox.Show("أختر النوع");
+                return;
+            }
+
+            ListViewItem item = new ListViewItem(new string[] { textBox1.Text });
+
+            item.Group = ChosenGroup();
+            
+            listView1.Items.Add(item);
+            string Line = textBox1.Text + "," + CheckedRadio().Name + Environment.NewLine;
+            if (Line != "")
+            {
+                File.AppendAllText(path, Line);
+            }
+            textBox1.Text = "";
+            CheckedRadio().Checked = false;
+        }
+    
+        void DeleteSelectedFromFile(ListViewItem Item)
+        {
+            string newLine = "";
+            string[] Lines = File.ReadAllLines(path);
+            foreach (string line in Lines)
+            {
+                string []records = line.Split(',');
+                if (records[0] != Item.SubItems[0].Text)
+                {
+                    newLine += line + Environment.NewLine;
+                }
+            }
+            File.WriteAllText(path, newLine);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            if(MessageBox.Show("هل أنت متأكد من الحذف؟","",MessageBoxButtons.OKCancel,MessageBoxIcon.Warning) == DialogResult.Cancel)
+            {
+                return;
+            }
+            foreach (ListViewItem item in listView1.SelectedItems)
+            {
+                item.Remove();
+                DeleteSelectedFromFile(item);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            LoadAllFileContentToListView();
+        }
+        string CopyTheFileDataNamesOnly()
+        {
+            string all = "";
+            string[] AllData = File.ReadAllLines(path);
+            for (int i = 0; i < AllData.Length; i++)
+            {
+                string[] records = AllData[i].Split(',');
+                records[0] += Environment.NewLine + Environment.NewLine;
+                all += records[0];
+            }
+            return all;
+        }
+        string CopyTheListViewDataNamesAndGroups()
+        {
+            string all = "";
+            string[] AllData = File.ReadAllLines(path);
+
+            all += "***************قرطاسية***************" + Environment.NewLine+ Environment.NewLine;
+            for (int i = 0; i < AllData.Length; i++)
+            {
+                string[] records = AllData[i].Split(',');
+
+                if (records[1] == "rbSchool")
+                {                    
+                    records[0] += Environment.NewLine + Environment.NewLine;
+                    all += records[0];
+                }    
+            }
+
+            all += "****************العاب*********************" + Environment.NewLine + Environment.NewLine;
+
+            for (int y = 0; y < AllData.Length; y++)
+            {
+                string[] records = AllData[y].Split(',');
+                
+                if (records[1] == "rbGames")
+                {
+
+                    records[0] += Environment.NewLine + Environment.NewLine;
+                    all += records[0];
+                }
+                
+            }
+            return all;
+        }
+    
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+
+            string all = CopyTheListViewDataNamesAndGroups();
+
+            System.Windows.Forms.Clipboard.SetText(all);
+            niCopied.BalloonTipText = "تم النسخ";
+            niCopied.ShowBalloonTip(10);
+            MessageBox.Show("تم النسخ");
+            
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+            desktopPath += "\\Copyمكتبة ادم ليست.txt";
+            File.WriteAllText(desktopPath, CopyTheListViewDataNamesAndGroups());
+           
+            Process.Start(desktopPath);
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+            desktopPath += "\\Copyمكتبة ادم ليست.txt";
+            File.Delete(desktopPath);
+        }
+    }
+}
